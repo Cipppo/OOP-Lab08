726040a5543984599c5dc40370ccbfdffb348740
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-16 12:31:25 +0100

    Implemented the graphical interface

--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,9 +1,15 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * A very simple program using a graphical interface.
@@ -12,6 +18,8 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
+    
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -51,12 +59,39 @@ public final class SimpleGUI {
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / 2, sh / 2);
+        
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        save.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.write("OK");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                
+            }
+        });
+        
         /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
          * flag makes the OS window manager take care of the default positioning
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+        frame.setLocationByPlatform(true);
+    }
+    
+    public static void main(final String[] args) {
+        new SimpleGUI();
     }
 
 }
